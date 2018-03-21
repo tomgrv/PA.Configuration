6bae1f1722d761cf0118675b37977cfeae7630f6
Thomas GERVAIS <thomas.gervais@st.com>
2018-03-21 12:00:41 +0100

    Finalize packaging

--- PA.ConfigurationProvider/Properties/AssemblyInfo.cs
+++ PA.ConfigurationProvider/Properties/AssemblyInfo.cs
@@ -1,12 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using PA.Utilities.AssemblyMetadata;
 
 // Les informations générales relatives à un assembly dépendent de 
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("PA.Configuration")]
+[assembly: AssemblyTitle("PA.ConfigurationProvider")]
 [assembly: AssemblyDescription("MEF catalog and provider for configuration items")]
+[assembly: AssemblyAuthors("tomgrv")]
+
 
 // L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
 // aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
